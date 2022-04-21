c6babfa104320619a6a4128e608dc8384a4364c0
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2022-04-21 19:55:05 +0100

    yt-dlp support

--- ytpl.py
+++ ytpl.py
@@ -1,40 +1,29 @@
 #!/usr/bin/env python3
 from yt_dlp import YoutubeDL
-import json
 import sys
+
+# tree walk to each, if ever they break the current format
+def dur(d):
+  if isinstance(d, dict):
+    if "duration" in d: yield int(d["duration"])
+    for k, v in d.items():
+      if isinstance(v, dict) and k != "fragments":
+        yield from dur(v)
+  elif isinstance(d, list):
+    for v in d:
+      if isinstance(v, dict):
+        yield from dur(v)
 
 def ytpl(url):
   "How long is a Youtube Playlist?"
-  opts = {"dump_single_json": True, "simulate": True, "quiet": True}
-  with YoutubeDL(opts) as ydl:
-    info = ydl.extract_info(url, download = False)
-    sani = ydl.sanitize_info(info)
-  with open("info.py", "w+", encoding = "utf8", newline = "\n") as f:
-    print(info, file = f)
-  with open("sani.py", "w+", encoding = "utf8", newline = "\n") as f:
-    print(sani, file = f)
-  # d = json.loads(info) # last stdout from ytdl
   
-  print(info["title"])
-  # print(d["title"])
-  
-  # tree walk to each, if ever they break the current format
-  def dur(d):
-    if isinstance(d, dict):
-      if "duration" in d: yield int(d["duration"])
-      for k, v in d.items():
-        if isinstance(v, dict) and k != "fragments":
-          yield from dur(v)
-    elif isinstance(d, list):
-      for v in d:
-        if isinstance(v, dict):
-          yield from dur(v)
-  
-  # seconds = sum(dur(d))
+  with YoutubeDL({"dump_single_json": True, "simulate": True, "quiet": True}) as ydl:
+    d = ydl.extract_info(url, download = False)
   
   # direct walk to durations in current format
   seconds = sum(v["duration"] for v in d["entries"])
   
+  print(d["title"])
   print(f"{seconds}s")
   minutes, seconds = seconds // 60, seconds % 60
   print(f"{minutes}m{seconds}s")
