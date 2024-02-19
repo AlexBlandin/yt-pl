7e39ac7117b92f66e4aeac3c5cd41b3eae3f910b
AlexBlandin <20842735+AlexBlandin@users.noreply.github.com>
2024-02-19 00:07:24 +0000

    The ruffening

--- ytpl.py
+++ ytpl.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
+"""
+How long is a Youtube Playlist?
+
+Copyright 2022 Alex Blandin
+"""
+
 import sys
 
 from yt_dlp import YoutubeDL
 
 
 # tree walk to each, if ever they break the current format
-def dur(d):
+def dur(d):  # noqa: ANN001, ANN201, D103
   if isinstance(d, dict):
     if "duration" in d:
       yield int(d["duration"])
@@ -18,11 +24,11 @@ def dur(d):
         yield from dur(v)
 
 
-def ytpl(url):
+def ytpl(url) -> None:  # noqa: ANN001
   """How long is a Youtube Playlist?"""
   with YoutubeDL({"dump_single_json": True, "simulate": True, "quiet": True}) as ydl:
     d = ydl.extract_info(url, download=False)
-  assert isinstance(d, dict)
+  assert isinstance(d, dict)  # noqa: S101
 
   # direct walk to durations in current format
   seconds = sum(v["duration"] for v in d["entries"])
@@ -31,13 +37,13 @@ def ytpl(url):
   print(f"{seconds}s")
   minutes, seconds = seconds // 60, seconds % 60
   print(f"{minutes}m{seconds}s")
-  if minutes >= 60:
+  if minutes >= 60:  # noqa: PLR2004
     hours, minutes = minutes // 60, minutes % 60
     print(f"{hours}h{minutes}m{seconds}s")
-    if hours >= 24:
+    if hours >= 24:  # noqa: PLR2004
       days, hours = hours // 24, hours % 24
       print(f"{days}d{hours}h{minutes}m{seconds}s")
-      if days >= 7:
+      if days >= 7:  # noqa: PLR2004
         weeks, days = days // 7, days % 7
         print(f"{weeks}w{days}d{hours}h{minutes}m{seconds}s")
   print()
